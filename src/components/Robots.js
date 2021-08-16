f5cdfd48a6ae507fa75bf750788010df46d40a38
Dennis Campos <dennisjr89@gmail.com>
2021-08-16 16:33:31 -0400

    added map of robots information

--- src/components/Robots.js
+++ src/components/Robots.js
@@ -4,33 +4,42 @@ import "./Robots.css";
 import Navigation from "./Navigation";
 
 const Robots = () => {
-  const [robots, setRobots] = useState(null)
+  const [robots, setRobots] = useState(null);
 
   const url = "https://mondo-robot-art-api.herokuapp.com/robots";
   const API_KEY = process.env.REACT_APP_API_KEY;
   const TOKEN = process.env.REACT_APP_TOKEN;
 
   useEffect(() => {
-    axios.get(url, {
-      headers: {
-        accept: "application/json",
-        "x-robot-art-api-key": API_KEY,
-        Authorization: "Bearer " + TOKEN,
-        "Content-Type": "application/json",
-      },
-    }).then((res) => {
-      setRobots(res.data)
-    })
-  }, [url, API_KEY, TOKEN])
+    axios
+      .get(url, {
+        headers: {
+          accept: "application/json",
+          "x-robot-art-api-key": API_KEY,
+          Authorization: "Bearer " + TOKEN,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        setRobots(res.data);
+      });
+  }, [url, API_KEY, TOKEN]);
 
-console.log(robots)
+  const robotsInformation = robots?.map((robot, i) => {
+    return (
+      <div className="robots-card">
+        <h2>{robot.name}</h2>
+        <img className="robots-img" src={robot.url} alt="" />
+        <button>Vote</button>
+      </div>
+    );
+  });
 
   return (
-    <div>
+    <div className="robots-page">
       <Navigation />
       <h1>ROBOTS PAGE</h1>
-      <div className="robots-card">
-      </div>
+      <div className="robots-container">{robotsInformation}</div>
     </div>
   );
 };
