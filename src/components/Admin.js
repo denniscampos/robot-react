ae29317ae4b86060fca1f6b3d9e4360b6db5ad2f
Dennis Campos <dennisjr89@gmail.com>
2021-08-18 14:10:22 -0400

    added api to results and admin page to render

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,13 +1,55 @@
-import React from "react";
-import Robots from "../components/Robots";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./Admin.css";
 import Navigation from "./Navigation";
-import RobotsPage from "./RobotsPage";
 
 const Admin = () => {
+  const [robots, setRobots] = useState(null);
+
+  const url = "https://mondo-robot-art-api.herokuapp.com/robots";
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  // grab user token
+  const userToken = window.localStorage.getItem("user-token");
+
+  // grab robots
+  useEffect(() => {
+    axios
+      .get(url, {
+        headers: {
+          accept: "application/json",
+          "x-robot-art-api-key": API_KEY,
+          Authorization: "Bearer " + userToken,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        setRobots(res.data);
+      });
+  }, [url, API_KEY, userToken]);
+
+  const robotsInformation = robots?.map((robot, i) => {
+    return (
+      <div className="robots-card" key={i}>
+        <h2>{robot.name[0].toUpperCase() + robot.name.slice(1)}</h2>
+        <img className="robots-img" src={robot.url} alt="Robots" />
+        <div className="btn">
+          <button className="btn-vote">Edit</button>
+          <button className="btn-vote">Delete</button>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <>
       <Navigation />
-      {/* <RobotsPage title="Admin" robotsInformation={robotsInformation} /> */}
+      <div className="robots-page">
+        <div className="robots">
+          <h1>Admin</h1>
+          <div className="robots-container">{robotsInformation}</div>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,16 +1,73 @@
-import React from "react";
-import RobotsPage from "../components/RobotsPage";
-import Navigation from "../components/Navigation";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Robots from "../components/Robots";
+import Navigation from "./Navigation";
+import RobotsPage from "./RobotsPage";
 
 const Results = () => {
+  const [robots, setRobots] = useState(null);
+
+  const url = "https://mondo-robot-art-api.herokuapp.com/robots";
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  // grab user token
+  const userToken = window.localStorage.getItem("user-token");
+
+  // grab robots
+  useEffect(() => {
+    axios
+      .get(url, {
+        headers: {
+          accept: "application/json",
+          "x-robot-art-api-key": API_KEY,
+          Authorization: "Bearer " + userToken,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        setRobots(res.data);
+      });
+  }, [url, API_KEY, userToken]);
+
+  const robotsInformation = robots?.map((robot, i) => {
+    return (
+      <div className="robots-card" key={i}>
+        <h2>{robot.name[0].toUpperCase() + robot.name.slice(1)}</h2>
+        <img className="robots-img" src={robot.url} alt="Robots" />
+        <div className="btn">
+          <span className="vote-count">0/55</span>
+          <span className="progress-bar"></span>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <>
       <Navigation />
-      <div>
-        <RobotsPage title="Results" />
+      <div className="robots-page">
+        <div className="robots">
+          <h1>Results</h1>
+          <div className="robots-container">{robotsInformation}</div>
+        </div>
       </div>
     </>
   );
 };
 
 export default Results;
+
+{
+  /* <div className="robots-card" key={i}>
+        <h2>{robot.name[0].toUpperCase() + robot.name.slice(1)}</h2>
+        <img className="robots-img" src={robot.url} alt="Robots" />
+        <button
+          className="btn-vote"
+          onClick={(e) => {
+            createVote(e, robot.id);
+          }}
+        >
+          Vote
+        </button>
+      </div> */
+}
